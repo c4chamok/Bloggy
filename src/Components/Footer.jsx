13490527bffedcc3140228e25f291502419489a8
c4chamok <c4chamok@gmail.com>
2025-01-24 14:37:01 +0600

    footer text added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="bg-gray-800 text-white py-4 mt-8">
         <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} Bloggy. All rights reserved.</p>
+          <p>&copy; 2025 Bloggy. All rights reserved.</p>
         </div>
       </footer>
     );
